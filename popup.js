cb8ffd1f294dc3d2fea79b90b10a63276e2af572
Isaac Wilcove <isaac@wilcove.com>
2017-10-04 22:16:02 -0700

    Made prettier and added trailing 0s

--- popup.js
+++ popup.js
@@ -1,4 +1,18 @@
 window.onload = setInterval(function() {
   var date = new Date();
-  document.getElementById("time").innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+  var time = [date.getHours(), date.getMinutes(), date.getSeconds(), "AM"]
+
+  if (time[0] > 12) {
+    time[0] -= 12;
+    time[3] = "PM";
+  }
+
+  for (var i = 0; i < time.length - 1; i++) {
+    if (time[i].toString().length == 1) {
+      time[i] = "0" + time[i];
+    }
+  }
+
+  document.getElementById("time").innerHTML = time[0] + ":" + time[1] + ":" + time[2] +  " " + time[3];
 }, 200);
