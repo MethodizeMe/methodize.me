e578a1eee4f7b56811d085fc7c0779fdb2e673a7
Isaac Wilcove <isaac@wilcove.com>
2017-10-05 18:06:12 -0700

    Added separate newtab and popup files

--- newtab.js
+++ newtab.js
@@ -1,7 +1,7 @@
 window.onload = setInterval(function() {
   var date = new Date();
 
-  var time = [date.getHours(), date.getMinutes(), "AM"]
+  var time = [date.getHours(), date.getMinutes(), date.getSeconds(), "AM"]
 
   if (time[0] > 12) {
     time[0] -= 12;
@@ -14,5 +14,5 @@ window.onload = setInterval(function() {
     }
   }
 
-  document.getElementById("time").innerHTML = time[0] + ":" + time[1] + " " + time[3];
+  document.getElementById("time").innerHTML = time[0] + ":" + time[1] + ":" + time[2] +  " " + time[3];
 }, 200);
